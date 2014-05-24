f54763a3aaf621e3ff56790c62845d4a50100f49
ide-satoru <mail@ide-satoru.net>
2014-05-25 06:46:20 +0900

    ファイル移動

--- js/setting.js
+++ js/setting.js
@@ -18,48 +18,55 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* susono cofog */
-  '20140100': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },/* 福祉の充実のために */
+  '20140100': { icon: 'icons/helping-others.svg', color: '#18c516', bcolor: '#935B3B' },/* 福祉の充実のために */
+  '20140101': { icon: 'icons/helping-others.svg', color: '#18C516', bcolor: '#935B3B' },/* 社会福祉費 */
+
+  '20140300': { icon: 'icons/misc-services.svg', color: '#18C516', bcolor: '#935B3B' },/* 総務費として */
+  '20140301': { icon: 'icons/misc-services.svg', color: '#18C516', bcolor: '#935B3B' },/* 総務費として */
+  '20140304': { icon: 'icons/helping-others.svg', color: '#18C516', bcolor: '#935B3B' },/* 選挙費 */
+
+  '20140400': { icon: 'icons/japanese-money.svg', color: '#18C516', bcolor: '#935B3B' },/* 借金の返済に */
+  '20140401': { icon: 'icons/japanese-money.svg', color: '#18C516', bcolor: '#935B3B' },/* 公債費 */
+
+  '20140500': { icon: 'icons/schools.svg', color: '#18C516', bcolor: '#935B3B' },/* 教育のために */
+  '20140501': { icon: 'icons/human-resources.svg', color: '#18C516', bcolor: '#935B3B' },/* 教育総務費 */
+  '20140502': { icon: 'icons/primary.svg', color: '#18C516', bcolor: '#935B3B' },/* 小学校費 */
+  '20140503': { icon: 'icons/post-secondary.svg', color: '#18C516', bcolor: '#935B3B' },/* 中学校費 */
+  '20140504': { icon: 'icons/pre-school.svg', color: '#18C516', bcolor: '#935B3B' },/* 幼稚園費 */
+  '20140505': { icon: 'icons/schools.svg', color: '#18C516', bcolor: '#935B3B' },/* 社会教育費 */
+  '20140506': { icon: 'icons/health.svg', color: '#18C516', bcolor: '#935B3B' },/* 保健体育費 */
+
+  '20140701': { icon: 'icons/farms.svg', color: '#18C516', bcolor: '#935B3B' },/* 農業費 */
 
 
 
-
-  '20140504': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#935B3B' },/* 幼稚園費 */
-
-
-
-  '20140701': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#935B3B' },/* 農業費 */
-
-
-
-  
-  
-  '1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '1-1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '2': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-1': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-2': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
-  '3': { icon: 'icons/economy-tourism.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-1': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-2': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-3': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '4': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#D33673' },
-  '4-1': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#D33673' },
-  '4-2': { icon: 'icons/street-lights.svg', color: '#C75746', bcolor: '#D33673' },
-  '4-3': { icon: 'icons/housing.svg', color: '#C75746', bcolor: '#D33673' },
-  '5': { icon: 'icons/environment.svg', color: '#C75746', bcolor: '#2A3A03' },
-  '5-1': { icon: 'icons/tree.svg', color: '#C75746', bcolor: '#2A3A03'  },
-  '5-2': { icon: 'icons/wind.svg', color: '#C75746', bcolor: '#2A3A03'  },
-  '6': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
-  '6-1': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
-  '7': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
-  '7-1': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
-  '8': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
-  '8-1': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#C75746' },
-  '8-2': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
-  '9': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
-  '9-1': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
-  '10': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
-  '10-1': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '1': { icon: 'icons/helping-others.svg', color: '#18C516', bcolor: '#935B3B' },
+  '1-1': { icon: 'icons/helping-others.svg', color: '#18C516', bcolor: '#935B3B' },
+  '2': { icon: 'icons/schools.svg', color: '#18C516', bcolor: '#0AB971' },
+  '2-1': { icon: 'icons/pre-school.svg', color: '#18C516', bcolor: '#0AB971' },
+  '2-2': { icon: 'icons/education.svg', color: '#18C516', bcolor: '#0AB971' },
+  '3': { icon: 'icons/economy-tourism.svg', color: '#18C516', bcolor: '#4E6D00' },
+  '3-1': { icon: 'icons/dollar.svg', color: '#18C516', bcolor: '#4E6D00' },
+  '3-2': { icon: 'icons/harbor.svg', color: '#18C516', bcolor: '#4E6D00' },
+  '3-3': { icon: 'icons/culture.svg', color: '#18C516', bcolor: '#4E6D00' },
+  '4': { icon: 'icons/our-streets.svg', color: '#18C516', bcolor: '#D33673' },
+  '4-1': { icon: 'icons/farms.svg', color: '#18C516', bcolor: '#D33673' },
+  '4-2': { icon: 'icons/street-lights.svg', color: '#18C516', bcolor: '#D33673' },
+  '4-3': { icon: 'icons/housing.svg', color: '#18C516', bcolor: '#D33673' },
+  '5': { icon: 'icons/environment.svg', color: '#18C516', bcolor: '#2A3A03' },
+  '5-1': { icon: 'icons/tree.svg', color: '#18C516', bcolor: '#2A3A03'  },
+  '5-2': { icon: 'icons/wind.svg', color: '#18C516', bcolor: '#2A3A03'  },
+  '6': { icon: 'icons/civilian-action.svg', color: '#18C516', bcolor: '#EC2406' },
+  '6-1': { icon: 'icons/civilian-action.svg', color: '#18C516', bcolor: '#EC2406' },
+  '7': { icon: 'icons/traffic-watersup.svg', color: '#18C516', bcolor: '#938626' },
+  '7-1': { icon: 'icons/traffic-watersup.svg', color: '#18C516', bcolor: '#938626' },
+  '8': { icon: 'icons/government.svg', color: '#18C516', bcolor: '#18C516' },
+  '8-1': { icon: 'icons/publicaffairs.svg', color: '#18C516', bcolor: '#18C516' },
+  '8-2': { icon: 'icons/government.svg', color: '#18C516', bcolor: '#18C516' },
+  '9': { icon: 'icons/garbage.svg', color: '#18C516', bcolor: '#D33673' },
+  '9-1': { icon: 'icons/garbage.svg', color: '#18C516', bcolor: '#D33673' },
+  '10': { icon: 'icons/order-safety.svg', color: '#18C516', bcolor: '#790586' },
+  '10-1': { icon: 'icons/order-safety.svg', color: '#18C516', bcolor: '#790586' }
 };
 
 
